631768616760201fe5f86c85cf5a1fe4f05dc377
Callum Dennis <callumdennisie@gmail.com>
2023-01-12 17:30:59 +0000

    refactor: use a Class rather than individual functions

--- run.py
+++ run.py
@@ -15,104 +15,112 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('timed_type_test_questions')
 
 
-def get_question(user_difficulty):
+class Game:
     """
-    Get a question from the Google Sheet, based on the difficulty selected by
-    the user.
-    """
-
-    # Dictionary to pair column numbers with difficulty
-    question_difficulty = {'easy': 1, 'hard': 2}
-
-    # Store the worksheet as a variable
-    questions = SHEET.worksheet('questions')
-
-    # Get the question from the correct difficulty, specified by the user
-    # Currently only taking the first non-header value in the column
-    question = questions.col_values(question_difficulty[user_difficulty])[1]
-
-    return question
-
-
-def get_input(question):
-    """
-    Ask the user to input the question and return the result
+    The class for each game session that is started.
     """
 
-    print("Type the following question, as quick as possible:")
-    print(question)
-    print()
-    answer = input("Type here:\n")
+    def __init__(self, max_time):
+        self.max_time = max_time
 
-    return answer
+    def get_question(self, user_difficulty):
+        """
+        Get a question from the Google Sheet, based on the difficulty selected 
+        by the user.
+        """
 
+        # Dictionary to pair column numbers with difficulty
+        question_difficulty = {'easy': 1, 'hard': 2}
 
-def calculate_accuracy(question, answer):
-    """
-    Calculate the similarity of the question and the answer and return as a
-    percentage
-    """
+        # Store the worksheet as a variable
+        questions = SHEET.worksheet('questions')
 
-    # Calculate Similarity
-    similarity = SequenceMatcher(lambda x: x == " ", question, answer)
+        # Get the question from the correct difficulty, specified by the user
+        # Currently only taking the first non-header value in the column
+        question = questions.col_values(
+            question_difficulty[user_difficulty])[1]
 
-    # Get the similarity score as a decimal
-    decimal = similarity.ratio()
+        return question
 
-    # Convert the decimal to a percentage
-    percentage = round(decimal * 100, 2)
+    def get_input(self, question):
+        """
+        Ask the user to input the question and return the result
+        """
 
-    return percentage
+        print("Type the following question, as quick as possible:")
+        print(question)
+        print()
+        answer = input("Type here:\n")
 
+        return answer
 
-def calculate_speed(start_time, answer_time, max_time):
-    """
-    Calculate the speed of the users answer, using the start and answer time,
-    then calculate the time remainign from the maxium time.
-    """
-    # Calculate time taken to answer the question
-    time_taken = answer_time - start_time
+    def calculate_accuracy(self, question, answer):
+        """
+        Calculate the similarity of the question and the answer and return as a
+        percentage
+        """
 
-    # Round the seconds taken to the nearest full second
-    time_taken = round(time_taken)
+        # Calculate Similarity
+        similarity = SequenceMatcher(lambda x: x == " ", question, answer)
 
-    time_left = max_time - time_taken
+        # Get the similarity score as a decimal
+        decimal = similarity.ratio()
 
-    # Calculate the percentage of time taken from max time
-    speed = round((time_taken / max_time) * 100, 2)
+        # Convert the decimal to a percentage
+        percentage = round(decimal * 100, 2)
 
-    # Reverse the percentage to get score out of 100
-    speed = 100 - speed
+        return percentage
 
-    return speed, time_taken, time_left
+    def calculate_speed(self, start_time, answer_time):
+        """
+        Calculate the speed of the users answer, using the start and answer 
+        time, then calculate the time remainign from the maxium time.
+        """
+        # Calculate time taken to answer the question
+        time_taken = answer_time - start_time
 
+        # Round the seconds taken to the nearest full second
+        time_taken = round(time_taken)
 
-def output_results(question, answer, time_left, time_taken, speed, accuracy):
-    """
-    Output the game results that are passed in.
-    """
-    print(f"\nQuestion: {question}\nAnswer: {answer}\n")
-    print(f"Time left: {time_left} Seconds")
-    print(f"Time taken: {time_taken} Seconds")
-    print(f"Speed: {speed}%")
-    print(f"\nAccuracy: {accuracy}%")
+        time_left = max_time - time_taken
+
+        # Calculate the percentage of time taken from max time
+        speed = round((time_taken / max_time) * 100, 2)
+
+        # Reverse the percentage to get score out of 100
+        speed = 100 - speed
+
+        return speed, time_taken, time_left
+
+    def output_results(self, question, answer, time_left, time_taken, speed, 
+                       accuracy):
+        """
+        Output the game results that are passed in.
+        """
+        print(f"\nQuestion: {question}\nAnswer: {answer}\n")
+        print(f"Time left: {time_left} Seconds")
+        print(f"Time taken: {time_taken} Seconds")
+        print(f"Speed: {speed}%")
+        print(f"\nAccuracy: {accuracy}%")
 
 
 max_time = 30
-user_question = get_question('hard')
+new_game = Game(max_time)
+
+user_question = new_game.get_question('hard')
 
 # The time before the question was asked
 start_time = time.time()
 
-user_input = get_input(user_question)
+user_input = new_game.get_input(user_question)
 
 # The time after the question was asked
 answer_time = time.time()
 
-accuracy = calculate_accuracy(user_question, user_input)
+accuracy = new_game.calculate_accuracy(user_question, user_input)
 
-speed, time_taken, time_left = calculate_speed(start_time, answer_time,
-                                               max_time)
+speed, time_taken, time_left = new_game.calculate_speed(start_time, 
+                                                        answer_time)
 
-output_results(user_question, user_input, time_left, time_taken, speed,
-               accuracy)
+new_game.output_results(user_question, user_input, time_left, time_taken, 
+                        speed, accuracy)
